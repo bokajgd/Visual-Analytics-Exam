a2a5fa463e96a971321fb0f47303b3cb189ab862
Jakob Grøhn <bokajgd@gmail.com>
2021-05-19 01:36:55 +0200

    Freshing up model scripts

--- A4-Image-Classification/src/models/cnn_mnist.py
+++ A4-Image-Classification/src/models/cnn_mnist.py
@@ -1,22 +1,18 @@
 # Classifying the MNIST handwritten digits (0-9)
 
 # Import packages
-import numpy as np # Matrix maths
-import tensorflow as tf # NN functions
 import matplotlib.pyplot as plt # For drawing graph
-from tensorflow.keras.models import Sequential # ANN Architecture
-from tensorflow.keras.layers import Dense # The layers in the ANN
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import classification_report
 import keras
-import os
 from pathlib import Path
 
 # Import utility functions definied in utils.py
 from models.model_utils.utils import draw_neural_net
 
-# Defining cnn in a single function
+# Defining cnn in a single function - the function takes arguments 'n_layers' and 'n_nodes' which are provided by GUI user
 def cnn_mnist(n_layers, n_nodes):
+
     # Setting model output directory 
     model_out_dir = Path.cwd() / 'A4-Image-Classification' / 'output' 
 
@@ -27,25 +23,38 @@ def cnn_mnist(n_layers, n_nodes):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     # Normalizing the images / normalize the pixels values to values between [-0.5 ; 0.5]
-    x_train = (x_train/255) - 0.5 # This gives best conditions for parameters optimisation
+    # This gives best conditions for parameters optimisation
+    x_train = (x_train/255) - 0.5 
+
     x_test = (x_test/255) - 0.5
 
     # Flatting the images / Flatten from 28x28 matrix to 1x784 dimensional column vector
     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+
     x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    
     input_shape = (28, 28, 1)
 
-    # Build the model
+    # Build the convolutional layers of the model + pooling layers and dropout
     model = keras.models.Sequential()
+
     model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu',
                                 input_shape=input_shape))
+
     model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+
     model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
     model.add(keras.layers.Dropout(0.25))
+
     model.add(keras.layers.Flatten())
-    # Add dense layers with the dimensions according to input
+
+    # Let user define hyperparams of fully connected layers
+    # Add dense layers with the dimensions according to input by user
     for layer in range(n_layers):
+
         model.add(keras.layers.Dense(n_nodes[layer], activation='relu'))
+
     model.add(keras.layers.Dense(10, activation='softmax'))
 
     # Compile the layers into one model
@@ -69,18 +78,21 @@ def cnn_mnist(n_layers, n_nodes):
         x_test,
         to_categorical(y_test)
     )
-    
 
     # Saving evaluation metrics
     predictions = model.predict(x_test)
+
     predictions = predictions.argmax(axis=1)
+
     cm = classification_report(to_categorical(y_test).argmax(axis=1), predictions)
 
     # Saving figure
     nodes_str = str(n_nodes).replace('[','').replace(']','').replace(' ', '')
+
     model.save(model_out_dir / f"{n_layers}-dense-{nodes_str}-nodes-CNN.model") 
 
     # Visualising network
+
     # Styling title
     title = {'family': 'serif',
             'color': '#4A5B65',
@@ -88,14 +100,26 @@ def cnn_mnist(n_layers, n_nodes):
             'size': 24,
             }
 
-    network_structure = n_nodes + [10] # Creating full network structure by adding the dimension of the output layer
-    fig = plt.figure(figsize=(7, 7)) # Creating figure
-    ax = fig.gca()
-    ax.axis('off')
-    draw_neural_net(ax, .1, .9, .1, .9, network_structure) # Running visualisation function
-    ax.set_title(f"{n_layers}-dense-{n_nodes}-nodes-CNN", fontdict = title, y = 0.9) # Setting unique titlie
-    plt.savefig(model_out_dir / f"{n_layers}-dense-{nodes_str}-nodes-CNN-viz.png") # Saving figure
+    # Creating full network structure by adding the dimension of the output layer
+    network_structure = n_nodes + [10] 
+    
+    # Creating figure
+    fig = plt.figure(figsize=(7, 7)) 
 
+    ax = fig.gca()
+
+    ax.axis('off')
+
+     # Running visualisation function
+    draw_neural_net(ax, .1, .9, .1, .9, network_structure)
+     
+    # Setting unique titlie
+    ax.set_title(f"{n_layers}-dense-{n_nodes}-nodes-CNN", fontdict = title, y = 0.9)
+
+    # Saving figure
+    plt.savefig(model_out_dir / f"{n_layers}-dense-{nodes_str}-nodes-CNN-viz.png") 
+
+    # Return results
     return cm
 
 
--- A4-Image-Classification/src/models/lr_mnist.py
+++ A4-Image-Classification/src/models/lr_mnist.py
@@ -1,11 +1,11 @@
 # Importing packages
-import sys,os
 import numpy as np
 from pathlib import Path
-import argparse
+
+
 # Import sklearn stuff
 from sklearn import metrics
-from sklearn.datasets import fetch_openml
+from sklearn.datasets import fetch_openml 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -15,20 +15,24 @@ from models.model_utils.utils import plot_coefs
 
 # Defining logistic regression in a single function
 def lr_mnist(pen, tol):
+
     # Setting model output directory 
     model_out_dir = Path.cwd() / 'A4-Image-Classification' / 'output' 
-
-    X, y = fetch_openml('mnist_784', version=1, return_X_y=True) # Load data
+    
+    # Load data
+    X, y = fetch_openml('mnist_784', version=1, return_X_y=True) 
     
     # Convert to arrays
     X = np.array(X) 
+
     y = np.array(y)
     
     #Predifine classes and number of classes
     classes = sorted(set(y))
+
     n_classes = len(classes)
     
-    # Splitting data
+    # Splitting data (using a total of 10000 images)
     X_train, X_test, y_train, y_test = train_test_split(X, 
                                                     y, 
                                                     train_size=7500, 
@@ -36,6 +40,7 @@ def lr_mnist(pen, tol):
     
     # Scaling the features
     X_train_scaled = X_train/255.0
+
     X_test_scaled = X_test/255.0
 
     # Training the model
@@ -52,5 +57,6 @@ def lr_mnist(pen, tol):
 
     # Creating image of coefs
     plot_coefs(clf.coef_, n_classes, model_out_dir / f"{pen}-penalty-{tol}-tol-nodes-LR-viz.png")
-
+    
+    # Return results
     return cm
